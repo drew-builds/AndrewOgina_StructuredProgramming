e95ec648b46ecc9f162f61645b5b7a434bfbf2a1
AndrewOgina <muregiandrew@gmail.com>
2025-11-18 22:52:07 +0300

    Task 3: String length

--- assignments/assignment_1/task_3/main.c
+++ assignments/assignment_1/task_3/main.c
@@ -5,11 +5,9 @@ int main()
 {
     char str[1000];
 
-    // Input a string from the user
     printf("Enter a string: ");
     scanf("%999s", str);
 
-    // Find and print the length of the string
     printf("Length of the string: %lu\n", strlen(str));
 
     return 0;
